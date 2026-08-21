d076357f69fbc7117c244a16ba5e02b11ca598bb
damian862 <damian@otooles.co.uk>
2026-08-21 15:22:13 +0100

    Filter Key School Dates by selected school

--- app-14.js
+++ app-14.js
@@ -8,8 +8,13 @@ function hideOrganisationAdmin(){document.querySelectorAll('.nav button').forEac
 function setSelect(id){let el=$(id),sid=currentSite();if(!el||!sid)return;if([...el.options].some(o=>o.value===sid)){el.value=sid;el.disabled=!ownerRole();}}
 function applyContextFilters(){let sid=currentSite();if(!sid)return;['rbSite','calSite','repSite','billSite','lgSite'].forEach(setSelect)}
 function renderDashboardContext(){let sid=currentSite();if($('k1'))$('k1').textContent=sid?1:S.length;if($('k4'))$('k4').textContent=sid?B.filter(x=>x.site_id===sid).length:B.length}
+function withContextYears(fn){let sid=currentSite(),all=Y;if(sid)Y=Y.filter(y=>y.site_id===sid);try{return fn()}finally{Y=all}}
 function schoolBanner(){if(ownerRole()||document.getElementById('siteSchoolBanner'))return;let sid=currentSite(),name=siteName(sid);if(!name)return;let main=document.querySelector('.main'),top=document.querySelector('.top');if(!main||!top)return;let b=document.createElement('div');b.id='siteSchoolBanner';b.className='site-school-banner';b.innerHTML=`<b>${e(name)}</b><span>Your account is restricted to this school's pool operations.</span>`;top.insertAdjacentElement('afterend',b)}
-const oldRender=window.render;window.render=function(){oldRender();addContextUi();schoolBanner();applyContextFilters();renderDashboardContext();hideOrganisationAdmin()};
+const oldTermDates=window.renderTermDates;window.renderTermDates=function(){return withContextYears(oldTermDates)};
+const oldEditYear=window.editYear;window.editYear=function(id){let out=oldEditYear(id),sid=currentSite(),site=$('f1');if(!id&&sid&&site)site.value=sid;return out};
+const oldEditTerm=window.editTerm;window.editTerm=function(...args){return withContextYears(()=>oldEditTerm(...args))};
+const oldEditDate=window.editDate;window.editDate=function(...args){return withContextYears(()=>oldEditDate(...args))};
+const oldRender=window.render;window.render=function(){oldRender();addContextUi();schoolBanner();applyContextFilters();renderTermDates();renderDashboardContext();hideOrganisationAdmin()};
 const oldRecurring=window.renderRecurringBookings;window.renderRecurringBookings=function(){oldRecurring();setTimeout(applyContextFilters,0)};
 const oldIncome=window.renderIncomeSummary;window.renderIncomeSummary=function(){oldIncome();setTimeout(applyContextFilters,0)};
 const oldCal=window.renderBookingCalendar;if(oldCal)window.renderBookingCalendar=function(){oldCal();setTimeout(applyContextFilters,0)};
